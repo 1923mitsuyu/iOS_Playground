ee59ef5b1bac04ea0f0e4c1e6a859164f743b6f9
Ikufumi Mitsuyu <Ikufumi665@gmail.com>
2026-06-14 13:03:35 +0900

    Initial commit

---
+++ iOS_Playground/iOS_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_PlaygroundApp.swift
+//  iOS_Playground
+//
+//  Created by Ikufumi Mitsuyu on 2026/06/14.
+//
+
+import SwiftUI
+
+@main
+struct iOS_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
