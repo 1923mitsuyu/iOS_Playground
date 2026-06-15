3aaa7ceed58e9b3dec84b39be2b4bf5bb7386461
Ikufumi Mitsuyu <Ikufumi665@gmail.com>
2026-06-16 00:04:19 +0900

    リファクタリング

--- PlaygroundCore/Sources/Samples/ExpandableScrollViewSample/ExpandableScrollView.swift
+++ PlaygroundCore/Sources/Samples/ExpandableScrollViewSample/ExpandableScrollView.swift
@@ -5,49 +5,66 @@
 //  Created by Ikufumi Mitsuyu on 2026/06/15.
 //
 
+// TODO
+// maxItemCountsの数が少ない場合は、Spacerで埋める
+// Contentの高さ < ScrollViewの高さの場合は Scrollを Disableにする
+
 import SwiftUI
 
 package struct ExpandableScrollView: View {
-  @State private var offset: CGFloat = 0
+  // MARK: - Properties
+  @State private var scale: CGFloat = 0
+  
+  private let maxItemCounts = 20
+  // MARK: - Initialize
   package init() {}
+  // MARK: - Body
   package var body: some View {
     ZStack(alignment: .top) {
       Group {
-        Color.blue
-        Text("このテキストの上をScrollViewが通過します。")
+        Color.white
+        VStack(spacing: 24) {
+          Text("ScrollViewが重なります。")
+          Text("Scroll Downすると、Viewが拡大します。")
+        }
+        .padding(.top, 100)
       }
-      .scaleEffect(
-        1 + max(0, -offset) / 300,
-        anchor: .bottom
-      )
+      .scaleEffect(scale, anchor: .bottom)
       ScrollView {
         Color.clear
           .frame(height: 300)
-        VStack(spacing: 16) {
-          ForEach(0..<100) { index in
+        VStack(spacing: 20) {
+          /// ForEachにidがない場合は、Non-constant range: argument must be an integer literalエラーが発生
+          /// https://blog.hikaru.run/2022/06/07/swiftui-foreach-warning.html
+          ForEach(0..<maxItemCounts, id: \.self) { index in
             Text("Item \(index)")
               .frame(maxWidth: .infinity)
-              .padding()
+              .frame(height: 60)
+              .background(.white)
+              .clipShape(.rect(cornerRadius: 30))
+              .padding(.horizontal, 60)
           }
         }
-        .scrollBounceBehavior(.basedOnSize)
-        .background(.red)
-        .clipShape(.rect(topLeadingRadius: 16, topTrailingRadius: 16))
+        .padding(.top, 20)
+        .padding(.bottom, 48)
+        .background(.gray)
+        .clipShape(.rect(topLeadingRadius: 30, topTrailingRadius: 30))
       }
       .onScrollGeometryChange(
         for: CGFloat.self,
         of: { geometry in
           geometry.contentOffset.y
         },
-        action: { _, newValue in
-          offset = newValue
+        action: { _, offset in
+          scale =  1 + max(0, -offset) / 600
         }
       )
     }
-    .ignoresSafeArea(edges: .top)
+    .ignoresSafeArea()
   }
 }
 
 #Preview {
   ExpandableScrollView()
 }
+
