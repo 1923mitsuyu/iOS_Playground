eff4d98ab1fb889a8d23ac2d7fb54f4d92a5cc47
Ikufumi Mitsuyu <Ikufumi665@gmail.com>
2026-06-15 12:32:12 +0900

    拡張スクロールビューの実装

--- PlaygroundCore/Sources/Samples/ExpandableScrollViewSample/ExpandableScrollView.swift
+++ PlaygroundCore/Sources/Samples/ExpandableScrollViewSample/ExpandableScrollView.swift
@@ -8,10 +8,44 @@
 import SwiftUI
 
 package struct ExpandableScrollView: View {
+  @State private var offset: CGFloat = 0
   package init() {}
   package var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    ZStack(alignment: .top) {
+      Group {
+        Color.blue
+        Text("このテキストの上をScrollViewが通過します。")
+      }
+      .scaleEffect(
+        1 + max(0, -offset) / 300,
+        anchor: .bottom
+      )
+      ScrollView {
+        Color.clear
+          .frame(height: 300)
+        VStack(spacing: 16) {
+          ForEach(0..<100) { index in
+            Text("Item \(index)")
+              .frame(maxWidth: .infinity)
+              .padding()
+          }
+        }
+        .scrollBounceBehavior(.basedOnSize)
+        .background(.red)
+        .clipShape(.rect(topLeadingRadius: 16, topTrailingRadius: 16))
+      }
+      .onScrollGeometryChange(
+        for: CGFloat.self,
+        of: { geometry in
+          geometry.contentOffset.y
+        },
+        action: { _, newValue in
+          offset = newValue
+        }
+      )
     }
+    .ignoresSafeArea(edges: .top)
+  }
 }
 
 #Preview {
